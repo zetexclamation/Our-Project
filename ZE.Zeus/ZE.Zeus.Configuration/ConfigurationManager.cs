34f9c6e2a439a916be8c92c0d7bbe5c1e072fe15
zetcore1 <dev.uk.core@gmail.com>
2018-03-10 07:25:34 +0530

    cm

--- ZE.Zeus/ZE.Zeus.Configuration/ConfigurationManager.cs
+++ ZE.Zeus/ZE.Zeus.Configuration/ConfigurationManager.cs
@@ -8,5 +8,34 @@ namespace ZE.Zeus.Configuration
 {
     public class ConfigurationManager
     {
+        public static string GetAppSetting(string configurationName)
+        {
+            return null;
+        }
+
+        public static string GetAppSetting(string configurationName, string defaultValue)
+        {
+            return null;
+        }
+
+        public static bool GetBoolSetting(string configurationName)
+        {
+            return false;
+        }
+
+        public static bool GetBoolSetting(string configurationName, string defaultValue)
+        {
+            return false;
+        }
+
+        public static string GetSetting(string configurationName)
+        {
+            return null;
+        }
+
+        public static string GetSetting(string configurationName, string defaultValue)
+        {
+            return null;
+        }
     }
 }
